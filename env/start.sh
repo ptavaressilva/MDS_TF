dff54b945181572f2cc319cefccc8007619ae4b7
Pedro Tavares <ptavaressilva@gmail.com>
2022-09-07 14:54:16 +0200

    Instrucciones para MLflow

--- env/start.sh
+++ env/start.sh
@@ -1,6 +1,6 @@
 export $(grep -v '^#' ./.env | xargs)
 
-docker run -it -p 8888:8888 \
+docker run -it -p 8888:8888 -p 5000:5050 \
            -v ${TF_MDS_repo1_host}:${TF_MDS_repo1_cont} \
            -v ${TF_MDS_repo2_host}:${TF_MDS_repo2_cont} \
            --name ds-TF_MDS \
